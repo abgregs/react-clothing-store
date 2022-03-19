2fed9aece7894bcc0d1e494b54595646f0a015de
Austin Gregersen <abgregs@gmail.com>
2022-03-19 13:52:39 -0600

    add basic routing setup and navigation bar component with nav links

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <div className='logo-container'>
+          <NavLink className='logo-link' to='/'>
+            <Logo />
+          </NavLink>
+        </div>
+        <div className='nav-links-container'>
+          <NavLink className='nav-link' to='/shop'>
+            Shop
+          </NavLink>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
