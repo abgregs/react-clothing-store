687c53f040aaa84074b1917f63bc5caf13860565
Austin Gregersen <abgregs@gmail.com>
2022-03-22 13:06:07 -0600

    add a new context for product data and a Shop and ProductCard component that use it

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,13 +4,11 @@ import {
   createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils';
 
-// actual stuff you want to access
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
-// wrapper that provides access
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
