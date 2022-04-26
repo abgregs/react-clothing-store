79050df71f991ae0e486d1b88f70722c3552ac34
Austin Gregersen <abgregs@gmail.com>
2022-04-25 22:53:44 -0600

    add style updates to cart dropdown and cart item

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,29 +1,40 @@
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { selectCartItems } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
+import { Fragment } from 'react';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
+
+  const handleCheckoutClick = () => {
+    dispatch(setIsCartOpen(false));
+  }
 
   return (
-    <div className={'cart-dropdown-container'}>
-      <div className='cart-items'>
-        {cartItems.length ? (
-          cartItems.map((item) => <CartItem key={item.id} item={item} />)
-        ) : (
-          <span className='empty-message'>Your cart is empty</span>
-        )}
-        <Link to='/checkout'>
-          <Button buttonType='default'>Go to Checkout</Button>
-        </Link>
+      <div className={'cart-dropdown-container'}>
+        <div className='cart-items'>
+          {cartItems.length ? (
+            <Fragment>
+              {cartItems.map((item) => (
+                <CartItem key={item.id} item={item} />
+              ))}
+              <Link to='/checkout'>
+                <Button buttonType='default' onClick={handleCheckoutClick}>Go to Checkout</Button>
+              </Link>
+            </Fragment>
+          ) : (
+            <span className='empty-message'>Your cart is empty</span>
+          )}
+        </div>
       </div>
-    </div>
   );
 };
 
